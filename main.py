098a054d6478c5e111b7a1bfc4009f5cf3cb7f9e
Albert Jokelin <albertjokelin@icloud.com>
2021-02-21 11:52:21 +0400

    Completed Project

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # This program is designed by Albert Jokelin.
-# This program analyses NASDAQ Companies and makes a highly profitable portfolio
-# The code for the scrapper was taken from https://towardsdatascience.com/downloading-historical-stock-prices-in-python-93f85f059c1f
+# This program analyses NASDAQ Companies and makes a highly profitable portfolio. If you want NSE, add those symbols to the Symbols list in scrapper.py
 # This project has been built for educational purposes, DO NOT use this to make your portfolio choices
-# If you use this software, any liability it brings will solely be your responisibiliy
-
+# If you use this software, any liability it brings will solely be your responisibiliy.
 # Import required libraries
 import pandas as pd
 import numpy as np
@@ -14,9 +12,67 @@ import datetime
 import time
 import io
 
+# To optimize the portfolio
+import cvxpy as cp
+from pypfopt import risk_models
+from pypfopt import expected_returns
+from pypfopt import EfficientFrontier
+from pypfopt import objective_functions
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 
-try:
-    df = pd.read_csv('C:\\scrapper.csv')
-except Exception:
-    print("CSV file not found")
+ 
+# Get the names of the companies
+def get_name(ticker):
+    url = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query='+ticker+'&region=1&lang=en'
+    result = requests.get(url).json()
+    for r in result['ResultSet']['Result']:
+        if r['symbol'] == ticker:
+            return r['name']
 
+if __name__=="__main__":
+    try:
+        df = pd.read_csv('scrapper.csv')
+    except Exception:
+        print("CSV file not found")
+
+
+    assets = df.columns
+
+    # Calculate the annual returns 
+    mu  = expected_returns.mean_historical_return(df) # Mean
+    S = risk_models.sample_cov(df) # Sample Covariance matrix
+
+
+    # Optimize for the maximal Sharpe Ratio
+    ef = EfficientFrontier(mu, S)
+    weights = ef.max_sharpe()
+
+    cleaned_weights = ef.clean_weights()
+
+    # Get the discrete allocation of each share per stock
+    portfolio_val = 5000 # How much money you want to invest in USD
+    latest_prices = get_latest_prices(df)
+    weights = cleaned_weights
+    da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=portfolio_val)
+    allocation, leftover = da.lp_portfolio() # Allocation in var-1 and leftover amount in var-2
+
+    # Store all company names in a list
+    company_name = []
+    discrete_allocation_list = []
+    for symbol in allocation:
+        company_name.append(get_name(symbol))
+        discrete_allocation_list.append(allocation.get(symbol))
+
+    # Create a dataframe for the portfolio
+    portfolio_df = pd.DataFrame(columns=['Company_name', 'Company_Ticker', 'Discrete_Val_'+str(portfolio_val)])
+
+    portfolio_df['Company_name'] = company_name
+    portfolio_df['Company_Ticker'] = allocation
+    portfolio_df['Discrete_Val_'+str(portfolio_val)] = discrete_allocation_list
+
+    # Display the portfolio
+    print(portfolio_df)
+    #print("\nDiscrete Allocation: ", allocation)
+    print("\nFunds remaining: $", leftover)
+    #print(cleaned_weights)
+    ef.portfolio_performance(verbose=False)
--- scrapper.py
+++ scrapper.py
@@ -9,73 +9,80 @@ import time
 import io
 # ---- WEB SCRAPER BEGIN -----
 # Set date
-start = datetime.datetime(2020,10,1)
+start = datetime.datetime(2015,10,1)
 end = datetime.datetime(2020,10,31)
 
 #Load and store the data
 Symbols = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET',
 		'AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE',
-		'AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP',
-		'AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANDV',
+		'ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP',
+		'AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','ADI','ANDV',
 		'ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','APTV','ADM','ARNC',
-		'AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BHGE','BLL','BAC','BK',
-		'BAX','BBT','BDX','BRK.B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX',
-		'BHF','BMY','AVGO','BF.B','CHRW','CA','COG','CDNS','CPB','COF','CAH','CBOE',
-		'KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW',
-		'CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG',
+		'AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BLL','BAC','BK',
+		'BAX','BDX','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX',
+		'BHF','BMY','AVGO','CHRW','CA','COG','CDNS','CPB','COF','CAH','CBOE',
+		'KMX','CCL','CAT','CNC','CNP','CERN','CF','SCHW',
+		'CHTR','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG',
 		'CTXS','CLX','CME','CMS','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP',
 		'ED','STZ','COO','GLW','COST','COTY','CCI','CSRA','CSX','CMI','CVS','DHI',
 		'DHR','DRI','DVA','DE','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH',
-		'DG','DLTR','D','DOV','DWDP','DPS','DTE','DRE','DUK','DXC','ETFC','EMN','ETN',
+		'DG','DLTR','D','DOV','DTE','DRE','DUK','DXC','EMN','ETN',
 		'EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR',
 		'ESS','EL','ES','RE','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST',
 		'FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F','FTV',
-		'FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GGP','GIS','GM','GPC','GILD',
-		'GPN','GS','GT','GWW','HAL','HBI','HOG','HRS','HIG','HAS','HCA','HCP','HP','HSIC',
+		'FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GIS','GM','GPC','GILD',
+		'GPN','GS','GT','GWW','HAL','HBI','HOG','HIG','HAS','HCA','HP','HSIC',
 		'HSY','HES','HPE','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','HII',
 		'IDXX','INFO','ITW','ILMN','IR','INTC','ICE','IBM','INCY','IP','IPG','IFF','INTU',
-		'ISRG','IVZ','IQV','IRM','JEC','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY',
-		'KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LEG','LEN','LUK',
+		'ISRG','IVZ','IQV','IRM','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY',
+		'KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LH','LRCX','LEG','LEN',
 		'LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM',
-		'MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','KORS','MCHP','MU',
-		'MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MYL','NDAQ',
+		'MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','MCHP','MU',
+		'MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','NDAQ',
 		'NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI',
-		'NBL','JWN','NSC','NTRS','NOC','NCLH','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE',
+		'JWN','NSC','NTRS','NOC','NCLH','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE',
 		'ORCL','PCAR','PKG','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE',
-		'PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR',
-		'PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RJF','RTN','O',
-		'RHT','REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SBAC',
+		'PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PFG','PG','PGR',
+		'PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RJF','O',
+		'REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SBAC',
 		'SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV',
-		'SPGI','SWK','SBUX','STT','SRCL','SYK','STI','SYMC','SYF','SNPS','SYY','TROW','TPR',
-		'TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','TRV',
+		'SPGI','SWK','SBUX','STT','SRCL','SYK','SYF','SNPS','SYY','TROW','TPR',
+		'TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TSCO','TDG','TRV',
 		'TRIP','FOXA','FOX','TSN','UDR','ULTA','USB','UAA','UA','UNP','UAL','UNH','UPS','URI',
-		'UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO',
-		'VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','HCN','WDC','WU','WRK','WY','WHR','WMB',
-		'WLTW','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
+		'UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','V','VNO',
+		'VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','WDC','WU','WRK','WY','WHR','WMB',
+		'WLTW','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
 
 
 # create empty dataframe
 stock_final = pd.DataFrame()
+stock_final = yf.download(Symbols, start=start, end=end, progress=True)
+stock_final.drop(columns=['Open', 'Close', 'High', 'Low', 'Volume'], axis=1, inplace=True)
+stock_final.stack().reset_index().rename(index=str, columns={"level_1": "Symbol"}).sort_values(['Symbol','Date'])
+stock_final = stock_final.dropna(how='any',axis=1) 
 # iterate over each symbol
-for i in Symbols:  
+# for i in Symbols:  
     
-    # print the symbol which is being downloaded
-    print( str(Symbols.index(i)) + str(' : ') + i, sep=',', end=',', flush=True)  
+#     # print the symbol which is being downloaded
+#     print( str(Symbols.index(i)) + str(' : ') + i, sep=',', end=',', flush=True)  
     
-    try:
-        # download the stock price 
-        stock = []
-        stock = yf.download(i,start=start, end=end, progress=False)
+#     try:
+#         # download the stock price 
+#         stock = []
+#         stock = yf.download(i,start=start, end=end, progress=False)
         
-        # append the individual stock prices 
-        if len(stock) == 0:
-            None
-        else:
-            stock['Name']=i
-            stock_final = stock_final.append(stock,sort=False)
-    except Exception:
-        None
+#         # append the individual stock prices 
+#         if len(stock) == 0:
+#             None
+#         else:
+#             stock['Name']=i
+# 			stock.drop(columns=['Open', 'Close', 'High', 'Low', 'Volume'], axis=1, inplace=True)
+#             stock_final = stock_final.append(stock,sort=False)
+#     except Exception:
+#         None
+stock_final.dropna(how="any", thresh=None, subset=None,inplace=False)
 
 stock_final.to_csv(r'scrapper.csv', index = False, header = True)
-print("\n\n\nSuccessfully scraped.\nCheck root directory for the file \"scrapper.csv\" and add it to the Stock_Portfolio_Maker folder")
+print("\n\n\nSuccessfully scraped.\nCheck root directory for the file \"scrapper.csv\" and add it to the Stock_Portfolio_Maker folder.")
+print("Open scrapper.csv and delete the first row that says \"Adj Close\".")
 # -------- WEB SCRAPER END ---------
